278a68b3979a8600b9da0d0b8264dabf2c22d8cf
jwc <yhjgoldhair@naver.com>
2016-06-08 08:07:11 -0700

    just train-with-rate-event dr

---
+++ train-with-rate-event/data/my dataset0.py
@@ -0,0 +1,73 @@
+import predictionio
+
+client = predictionio.EventClient(
+  access_key="EFb6fVppPuBn8xZng2X95NN4A7S4GFfcHtLOLUqixFYxutKtVrBv0LB1wePvGP7C",
+  url="http://localhost:7070",
+  threads=5,
+  qsize=500
+)
+
+# Create a new user
+
+user_ids = ["u%s" % i for i in range(0, 4)]
+for user_id in user_ids:
+    print "Set user", user_id
+    client.create_event(
+        event="$set",
+        entity_type="user",
+        entity_id=user_id
+    )
+
+
+# Create a new item
+item_ids = ["i%s" % i for i in range(0, 6)]
+for item_id in item_ids:
+    print "Set item", item_id
+    client.create_event(
+        event="$set",
+        entity_type="item",
+        entity_id=item_id
+    )
+
+# def a new rating
+
+def rating(user, item, rate):
+    client.create_event(event="rate", entity_type="user", entity_id=user, target_entity_type="item",
+    target_entity_id=item, properties={"rating" : rate})
+print "def"
+# create a new rating
+
+rating("u0", "i0", 4)
+rating("u0", "i1", 4)
+rating("u0", "i2", 4)
+rating("u0", "i3", 2)
+rating("u0", "i4", 2)
+rating("u0", "i5", 2)
+
+rating("u1", "i0", 4)
+rating("u1", "i1", 4)
+rating("u1", "i2", 4)
+rating("u1", "i3", 2)
+rating("u1", "i4", 2)
+rating("u1", "i5", 2)
+
+rating("u2", "i0", 2)
+rating("u2", "i1", 2)
+rating("u2", "i2", 2)
+rating("u2", "i3", 4)
+rating("u2", "i4", 4)
+rating("u2", "i5", 4)
+
+rating("u3", "i0", 2)
+rating("u3", "i1", 2)
+rating("u3", "i2", 2)
+rating("u3", "i3", 4)
+rating("u3", "i4", 4)
+rating("u3", "i5", 4)
+
+rating("u4", "i1", 4)
+rating("u4", "i2", 4)
+rating("u4", "i3", 2)
+rating("u4", "i4", 2)
+
+print "rating"
